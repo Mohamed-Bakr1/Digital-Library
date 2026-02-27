8d68be1b484e13a463416d7a86db6f8cde2cf9f4
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-02-27 21:32:31 +0200

    Create DigitaLibraryDbContext

---
+++ Infrastructure/Persistence/DigitalLibraryDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence
+{
+    public class DigitalLibraryDbContext : DbContext
+    {
+        public DigitalLibraryDbContext(DbContextOptions<DigitalLibraryDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
