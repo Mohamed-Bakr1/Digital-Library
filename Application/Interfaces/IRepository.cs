19fd10cf6643e2d6c900c9671751331c4b9dc57f
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-03-22 19:00:11 +0200

    Create IRepository as a generic interface

---
+++ Application/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task AddAsync(TEntity entity);
+        Task<TEntity> GetByIdAsync(string Id);
+        IQueryable<TEntity> GetAll();
+        void Update(TEntity entity);
+        Task DeleteByIdAsync(string Id);
+    }
+}
