a818b0f2f05548ff08daf462345111f2bd36ef61
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-03-22 19:02:20 +0200

    Create a Repository that implement the Generic IRepository interface

---
+++ Infrastructure/Persistence/Repository.cs
@@ -0,0 +1,43 @@
+﻿using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private DigitalLibraryDbContext _context;
+        private DbSet<TEntity> _entity;
+
+        public Repository(DigitalLibraryDbContext context)
+        {
+            _context = context;
+            _entity = _context.Set<TEntity>();
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _entity.AddAsync(entity);
+        }
+
+        public async Task DeleteByIdAsync(string Id)
+        {
+            var entity = await _entity.FindAsync(Id);
+            _entity.Remove(entity);
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
